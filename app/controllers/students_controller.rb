fd62dc646bf64ec25dba12f7a60ca52add3560c7
Elle Faraday <faraday.elle@gmail.com>
2018-05-15 21:46:22 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,7 @@ class StudentsController < ApplicationController
   def edit
     set_student
     @student.active = !@student.active
+    redirect_to student_path(@student)
   end
 
   #def update
