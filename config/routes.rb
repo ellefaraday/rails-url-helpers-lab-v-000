7989a7e33bdd4aab3d9bc5b28c9d14f4148e7cba
Elle Faraday <faraday.elle@gmail.com>
2018-05-15 21:09:22 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :students, only: [:index, :show]
 
   get '/activate/:id', to: 'students#update', as: 'activate_student'
-  patch '/activate/:id', to: 'students#update', as: 'activate_student'
+  patch '/activate/:id', to: 'students#update'
 
   # This show route can be refactored into the above resources method call, like so:
   # resources :students, only: [:index, :show]
